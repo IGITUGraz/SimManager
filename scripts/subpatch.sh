ca213eed363be5361e16857fafb94ed154297800
Arjun Rao <arjun210493@gmail.com>
2018-06-18 17:33:40 +0200

    Bugfix in subpatch.sh, regarding subdirectories

--- scripts/subpatch.sh
+++ scripts/subpatch.sh
@@ -6,7 +6,7 @@ separator="----8<----8<----8<----8<----"
 function make_chunk {
     cd $1
     if ! git diff-index --quiet HEAD -- . ':(exclude)*.ipynb'; then
-        echo "${PWD#$root}/"
+        echo "${PWD#$root}/"  # get current directory relative to directory top
         git diff-index --patch --no-color HEAD -- . ':(exclude)*.ipynb'
         echo "$separator"
     fi
@@ -14,14 +14,28 @@ function make_chunk {
 
 case "$1" in
     "make" )
-        root=$PWD
-        make_chunk $PWD
+        root=$(git rev-parse --show-toplevel)
+
+        # exit if not in a repository
+        git_error_code="$?"
+        if [[ $git_error_code -ne 0 ]]; then
+            exit $git_error_code
+        fi
+
+        make_chunk $root
         for module in $(git submodule  foreach --recursive 'echo $toplevel/$path' | grep -v Entering); do
             make_chunk $module
         done
     ;;
     "apply")
-        root=$PWD
+        root=$(git rev-parse --show-toplevel)
+
+        # exit if not in a repository
+        git_error_code="$?"
+        if [[ $git_error_code -ne 0 ]]; then
+            exit $git_error_code
+        fi
+
         IFS=''
         while read pwd; do
             patch=""
